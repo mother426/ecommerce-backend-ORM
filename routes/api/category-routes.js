d8e9c823c200fee0ba0abd04c8dba1717ffd6dbd
John Kim <johnkimmsterr@gmail.com>
2021-03-19 19:09:13 -0400

    removed double category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Category }, { model: Product }],
+      include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
   } catch (err) {
